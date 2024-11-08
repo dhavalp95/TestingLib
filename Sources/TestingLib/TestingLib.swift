0bc7ec13771dd1c74eedee6c19391a03df4bd175
dhavalp95 <patoliyadhaval555@gmail.com>
2024-11-08 15:11:25 +0530

    Add Logic File

--- Sources/TestingLib/TestingLib.swift
+++ Sources/TestingLib/TestingLib.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+
+class TestingLib: UIView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .blue
+    }
+}
